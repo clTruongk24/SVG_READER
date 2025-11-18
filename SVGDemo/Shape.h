882cd1b010c831dfde68c58c76fb025365b83881
quanghuy1848 <nguyenquanghuy020106@gmail.com>
2025-11-18 21:40:25 +0700

    Update Shape.h

--- SVGDemo/Shape.h
+++ SVGDemo/Shape.h
@@ -5,6 +5,31 @@
 #include <string>
 #include <gdiplus.h>
 
+class Transform { //thêm class transform
+private:
+    float scaleX = 1.0f;
+    float scaleY = 1.0f;
+    float rotation = 0.0f;
+    float translateX = 0.0f;
+    float translateY = 0.0f;
+
+public:
+    void initTransform(float sX, float sY, float r, float tX, float tY) {
+        scaleX = sX;
+        scaleY = sY;
+        rotation = r;
+        translateX = tX;
+        translateY = tY;
+    }
+
+    void apply(Matrix& mtx) const {
+        mtx.Reset();
+        mtx.Scale(scaleX, scaleY);
+        mtx.Rotate(rotation);
+        mtx.Translate(translateX, translateY);
+    }
+};
+
 class Shape {
 	std::string id;
 	Gdiplus::Color stroke_color;
