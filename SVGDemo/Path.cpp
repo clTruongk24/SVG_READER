9044f95af47a1adc7c79e4546d20d2e8e5f0b49a
TruongK24 <24120480@student.hcmus.edu.vn>
2025-12-15 08:00:29 +0700

    feat: 'S' and 's' handle

--- SVGDemo/Path.cpp
+++ SVGDemo/Path.cpp
@@ -56,53 +56,171 @@ void path::parseD(const string& raw_d) {
     replace(d.begin(), d.end(), ',', ' ');
 
     stringstream ss(d);
-    char cmd;
+    char cmd = '\0';
+    char lastCMD = '\0';
 
     gPath->Reset();
 
-    while (ss >> cmd) {
+	float lastControlX = 0.0f;
+	float lastControlY = 0.0f;
+    bool hadControlPoint = false;
+
+    while (ss >> ws) {
+
+        if (isalpha(ss.peek())) {
+            ss >> cmd;
+			lastCMD = cmd;
+        }
+        else {
+			cmd = lastCMD;
+        }
 
         switch (cmd) {
-        case 'M': case 'm': {
+        case 'M': {
             float x, y;
             if (ss >> x >> y) {
                 gPath->StartFigure();
                 currentX = x;
                 currentY = y;
+                lastCMD = 'L';
+				hadControlPoint = false;
             }
             break;
         }
-        case 'L': case 'l': {
+        case 'm': {
+            float x, y;
+            if (ss >> x >> y) {
+                gPath->StartFigure();
+                currentX += x;
+                currentY += y;
+                lastCMD = 'l';
+				hadControlPoint = false;
+            }
+            break;
+		}
+        case 'L': {
             float x, y;
             if (ss >> x >> y) {
                 gPath->AddLine(currentX, currentY, x, y);
                 currentX = x;
                 currentY = y;
+				hadControlPoint = false;
             }
             break;
         }
-        case 'H': case 'h': { 
+        case 'l': {
+            float x, y;
+            if (ss >> x >> y) {
+                gPath->AddLine(currentX, currentY, currentX + x, currentY + y);
+                currentX += x;
+                currentY += y;
+				hadControlPoint = false;
+            }
+            break;
+		}
+        case 'H':{ 
             float x;
             if (ss >> x) {
                 gPath->AddLine(currentX, currentY, x, currentY);
                 currentX = x;
+				hadControlPoint = false;
             }
             break;
         }
-        case 'V': case 'v': { 
+        case 'h': {
+            float x;
+            if (ss >> x) {
+                gPath->AddLine(currentX, currentY, currentX + x, currentY);
+                currentX += x;
+				hadControlPoint = false;
+            }
+            break;
+        }
+        case 'V': { 
             float y;
             if (ss >> y) {
                 gPath->AddLine(currentX, currentY, currentX, y);
                 currentY = y;
+				hadControlPoint = false;
             }
             break;
         }
-        case 'C': case 'c': { 
+        case 'v': { 
+            float y;
+            if (ss >> y) {
+                gPath->AddLine(currentX, currentY, currentX, currentY + y);
+                currentY += y;
+				hadControlPoint = false;
+            }
+            break;
+		}
+        case 'C': { 
             float x1, y1, x2, y2, x3, y3;
             if (ss >> x1 >> y1 >> x2 >> y2 >> x3 >> y3) {
                 gPath->AddBezier(currentX, currentY, x1, y1, x2, y2, x3, y3);
+				lastControlX = x2;
+				lastControlY = y2;
                 currentX = x3;
                 currentY = y3;
+				hadControlPoint = true;
+            }
+            break;
+        }
+        case 'c' : { 
+            float x1, y1, x2, y2, x3, y3;
+            if (ss >> x1 >> y1 >> x2 >> y2 >> x3 >> y3) {
+                gPath->AddBezier(currentX, currentY, currentX + x1, currentY + y1,
+                                 currentX + x2, currentY + y2,
+                                 currentX + x3, currentY + y3);
+				lastControlX = currentX + x2;
+				lastControlY = currentY + y2;
+                currentX += x3;
+                currentY += y3;
+				hadControlPoint = true;
+            }
+            break;
+		}
+        case 'S': {
+            float x2, y2, x, y;
+            if (ss >> x2 >> y2 >> x >> y) {
+                float x1, y1;
+                if (hadControlPoint) {
+                    x1 = 2 * currentX - lastControlX;
+                    y1 = 2 * currentY - lastControlY;
+                }
+                else {
+                    x1 = currentX;
+                    y1 = currentY;
+                }
+				gPath->AddBezier(currentX, currentY, x1, y1, x2, y2, x, y);
+                lastControlX = x2;
+                lastControlY = y2;
+                currentX = x;
+				currentY = y;
+				hadControlPoint = true;
+            }
+            break;
+        }
+        case 's': {
+            float x2, y2, x, y;
+            if (ss >> x2 >> y2 >> x >> y) {
+                float x1, y1;
+                if (hadControlPoint) {
+                    x1 = 2 * currentX - lastControlX;
+                    y1 = 2 * currentY - lastControlY;
+                }
+                else {
+                    x1 = currentX;
+                    y1 = currentY;
+                }
+                lastControlX = currentX + x2;
+                lastControlY = currentY + y2;
+                gPath->AddBezier(currentX, currentY, x1, y1,
+                    currentX + x2, currentY + y2,
+                    currentX + x, currentY + y);
+                currentX += x;
+                currentY += y;
+                hadControlPoint = true;
             }
             break;
         }
