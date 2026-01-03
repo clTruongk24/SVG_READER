aa9bd13168f518633f010b8681aceda39f5a7736
TruongK24 <24120480@student.hcmus.edu.vn>
2026-01-03 09:34:03 +0700

    refactor: transform element only use matrix

--- SVGDemo/Transform.cpp
+++ SVGDemo/Transform.cpp
@@ -1,13 +1,191 @@
+//#include "stdafx.h"
+//#include "Transform.h"
+//#include <sstream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//Transform::Transform(float sx, float sy, float rot, float tx, float ty)
+//    : scaleX(sx), scaleY(sy),
+//    rotation(rot), translateX(tx), translateY(ty), matrix(nullptr), useMatrix(false) {
+//}
+//
+//Transform::~Transform() {
+//    if (matrix) {
+//        delete matrix;
+//        matrix = nullptr;
+//    }
+//}
+//
+//void Transform::Apply(Gdiplus::Graphics& g) const
+//{
+//    if (useMatrix && matrix) {
+//        g.MultiplyTransform(matrix);
+//    }
+//    else {
+//        //1. Translate
+//        if (translateX != 0.0f || translateY != 0.0f) {
+//            g.TranslateTransform(translateX, translateY);
+//        }
+//
+//        //2. Rotate
+//        if (rotation != 0.0f) {
+//            g.RotateTransform(rotation);
+//
+//        }
+//        //3. Scale
+//        if (scaleX != 1.0f || scaleY != 1.0f) {
+//            g.ScaleTransform(scaleX, scaleY);
+//        }
+//    }
+//}
+//
+//void Transform::setScaleX(float sx) {
+//    this->scaleX = sx;
+//}
+//
+//void Transform::setScaleY(float sy) {
+//    this->scaleY = sy;
+//}
+//
+//void Transform::setRotation(float rot) {
+//    this->rotation = rot;
+//}
+//
+//void Transform::setRotation(float rot, float cx, float cy) {
+//    if (!matrix) {
+//        matrix = new Gdiplus::Matrix();
+//    }
+//
+//    matrix->Translate(cx, cy);
+//    matrix->Rotate(rot);
+//    matrix->Translate(-cx, -cy);
+//    useMatrix = true;
+//}
+//
+//void Transform::setTranslateX(float tx) {
+//    this->translateX = tx;
+//}
+//
+//void Transform::setTranslateY(float ty) {
+//    this->translateY = ty;
+//}
+//
+//void Transform::setMatrix(float m11, float m12, float m21, float m22, float dx, float dy) {
+//    if (!matrix) {
+//        matrix = new Gdiplus::Matrix();
+//    }
+//    matrix->SetElements(m11, m12, m21, m22, dx, dy);
+//    useMatrix = true;
+//}
+//
+//Transform* Transform::parse(const std::string& transformStr) {
+//    if (transformStr.empty()) {
+//        return nullptr;
+//    }
+//
+//    Transform* transform = new Transform();
+//
+//    vector<string> tokens = split(transformStr);
+//
+//    for (auto& token : tokens) {
+//        int start = token.find('(');
+//        int end = token.find(')');
+//        if (start == string::npos || end == string::npos || end <= start) continue;
+//        string cmd = token.substr(0, start);
+//        string paramsStr = token.substr(start + 1, end - start - 1);
+//
+//        vector<float> params = parseParams(paramsStr);
+//
+//        transform->switchTransform(cmd, params);
+//    }
+//
+//    return transform;
+//}
+//
+//void Transform::switchTransform(const string& cmd, const vector<float>& params) {
+//    if (cmd == "translate" && (params.size() == 1 || params.size() == 2)) {
+//        float tx = params[0];
+//        float ty = (params.size() == 2) ? params[1] : 0.0f;
+//        setTranslateX(tx);
+//        setTranslateY(ty);
+//    }
+//    else if (cmd == "scale" && (params.size() == 1 || params.size() == 2)) {
+//        float sx = params[0];
+//        float sy = (params.size() == 2) ? params[1] : sx;
+//        setScaleX(sx);
+//        setScaleY(sy);
+//    }
+//    else if (cmd == "rotate" && (params.size() == 1 || params.size() == 3)) {
+//        float angle = params[0];
+//        if (params.size() == 3) {
+//            float cx = params[1];
+//            float cy = params[2];
+//            setRotation(angle, cx, cy);
+//        }
+//        else {
+//            setRotation(angle);
+//        }
+//    }
+//    else if (cmd == "matrix" && params.size() == 6) {
+//        setMatrix(params[0], params[1], params[2], params[3], params[4], params[5]);
+//    }
+//}
+//
+//vector<float> parseParams(const string& paramsStr) {
+//    vector<float> params;
+//    istringstream ss(paramsStr);
+//    string param;
+//    while (ss >> param) {
+//        params.push_back(stof(param));
+//    }
+//    return params;
+//}
+//
+//vector<string> split(const string& str) {
+//    vector<string> tokens;
+//
+//    int i = 0;
+//    while (i < str.length()) {
+//
+//        i = str.find_first_not_of(" \t\n", i);
+//        if (i == string::npos) break;
+//
+//        int j = str.find_first_of(" (", i);
+//        if (j == string::npos) break;
+//
+//        int k = str.find_first_of(")", j);
+//        if (k == string::npos) break;
+//
+//        string cmd = str.substr(i, k - i + 1);
+//        replace(cmd.begin(), cmd.end(), ',', ' ');
+//        tokens.push_back(cmd);
+//        i = k + 1;
+//    }
+//    return tokens;
+//}
+
 #include "stdafx.h"
 #include "Transform.h"
 #include <sstream>
 #include <algorithm>
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 using namespace std;
 
-Transform::Transform(float sx, float sy, float rot, float tx, float ty)
-    : scaleX(sx), scaleY(sy),
-    rotation(rot), translateX(tx), translateY(ty), matrix(nullptr), useMatrix(false) {}
+
+Transform::Transform() : matrix(nullptr), useMatrix(false) {
+}
+
+Transform::Transform(const Transform& other) : matrix(nullptr), useMatrix(other.useMatrix) {
+    if (other.matrix) {
+        matrix = other.matrix->Clone();
+    }
+}
 
 Transform::~Transform() {
     if (matrix) {
@@ -16,150 +194,225 @@ Transform::~Transform() {
     }
 }
 
-void Transform::Apply(Gdiplus::Graphics& g) const
-{
+void Transform::ensureMatrix() {
+    if (!matrix) {
+        matrix = new Gdiplus::Matrix();
+        useMatrix = true;
+    }
+}
+
+void Transform::Apply(Gdiplus::Graphics& g) const {
     if (useMatrix && matrix) {
         g.MultiplyTransform(matrix);
     }
-    else {
-        //1. Translate
-        if (translateX != 0.0f || translateY != 0.0f) {
-            g.TranslateTransform(translateX, translateY);
-        }
+}
 
-        //2. Rotate
-        if (rotation != 0.0f) {
-            g.RotateTransform(rotation);
+void Transform::addTranslate(float tx, float ty) {
+    ensureMatrix();
+    // In SVG, transforms are applied right-to-left in the transform string
+    // but when building the matrix, we multiply on the right (prepend)
+    Gdiplus::Matrix temp;
+    temp.Translate(tx, ty);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
 
+void Transform::addScale(float sx, float sy) {
+    ensureMatrix();
+    Gdiplus::Matrix temp;
+    temp.Scale(sx, sy);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::addRotate(float angle) {
+    ensureMatrix();
+    Gdiplus::Matrix temp;
+    temp.Rotate(angle);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::addRotate(float angle, float cx, float cy) {
+    ensureMatrix();
+    // Rotate around a point: translate to origin, rotate, translate back
+    Gdiplus::Matrix temp;
+    temp.Translate(cx, cy);
+    temp.Rotate(angle);
+    temp.Translate(-cx, -cy);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::addMatrix(float m11, float m12, float m21, float m22, float dx, float dy) {
+    ensureMatrix();
+    Gdiplus::Matrix temp(m11, m12, m21, m22, dx, dy);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::addSkewX(float angle) {
+    ensureMatrix();
+    float tanAngle = tan(angle * M_PI / 180.0f);
+    Gdiplus::Matrix temp(1.0f, 0.0f, tanAngle, 1.0f, 0.0f, 0.0f);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::addSkewY(float angle) {
+    ensureMatrix();
+    float tanAngle = tan(angle * M_PI / 180.0f);
+    Gdiplus::Matrix temp(1.0f, tanAngle, 0.0f, 1.0f, 0.0f, 0.0f);
+    matrix->Multiply(&temp, Gdiplus::MatrixOrderPrepend);
+}
+
+void Transform::parseTransformFunction(const string& func, const vector<float>& params) {
+    if (func == "translate") {
+        if (params.size() == 1) {
+            addTranslate(params[0], 0.0f);
         }
-        //3. Scale
-        if (scaleX != 1.0f || scaleY != 1.0f) {
-            g.ScaleTransform(scaleX, scaleY);
+        else if (params.size() == 2) {
+            addTranslate(params[0], params[1]);
         }
     }
-}
-
-void Transform::setScaleX(float sx) {
-    this->scaleX = sx;
-}
-
-void Transform::setScaleY(float sy) {
-    this->scaleY = sy;
-}
-
-void Transform::setRotation(float rot) {
-    this->rotation = rot;
-}
-
-void Transform::setRotation(float rot, float cx, float cy) {
-    if (!matrix) {
-        matrix = new Gdiplus::Matrix();
-	}
-
-    matrix->Translate(cx, cy);
-	matrix->Rotate(rot);
-    matrix->Translate(-cx, -cy);
-	useMatrix = true;
-}
-
-void Transform::setTranslateX(float tx) {
-    this->translateX = tx;
-}
-
-void Transform::setTranslateY(float ty) {
-    this->translateY = ty;
-}
-
-void Transform::setMatrix(float m11, float m12, float m21, float m22, float dx, float dy) {
-    if (!matrix) {
-        matrix = new Gdiplus::Matrix();
+    else if (func == "scale") {
+        if (params.size() == 1) {
+            addScale(params[0], params[0]);
+        }
+        else if (params.size() == 2) {
+            addScale(params[0], params[1]);
+        }
     }
-    matrix->SetElements(m11, m12, m21, m22, dx, dy);
-    useMatrix = true;
+    else if (func == "rotate") {
+        if (params.size() == 1) {
+            addRotate(params[0]);
+        }
+        else if (params.size() == 3) {
+            addRotate(params[0], params[1], params[2]);
+        }
+    }
+    else if (func == "skewX") {
+        if (params.size() == 1) {
+            addSkewX(params[0]);
+        }
+    }
+    else if (func == "skewY") {
+        if (params.size() == 1) {
+            addSkewY(params[0]);
+        }
+    }
+    else if (func == "matrix") {
+        if (params.size() == 6) {
+            addMatrix(params[0], params[1], params[2], params[3], params[4], params[5]);
+        }
+    }
 }
 
 Transform* Transform::parse(const std::string& transformStr) {
     if (transformStr.empty()) {
         return nullptr;
-	}
+    }
 
     Transform* transform = new Transform();
+    vector<string> tokens = splitTransforms(transformStr);
 
-	vector<string> tokens = split(transformStr);
+    for (const auto& token : tokens) {
+        size_t parenPos = token.find('(');
+        if (parenPos == string::npos) continue;
 
-    for (auto& token : tokens) {
-		int start = token.find('(');
-		int end = token.find(')');
-        if (start == string::npos || end == string::npos || end <= start) continue;
-		string cmd = token.substr(0, start);
-		string paramsStr = token.substr(start + 1, end - start - 1);
+        size_t closeParenPos = token.find(')', parenPos);
+        if (closeParenPos == string::npos) continue;
 
-		vector<float> params = parseParams(paramsStr);
+        string func = token.substr(0, parenPos);
+        string paramsStr = token.substr(parenPos + 1, closeParenPos - parenPos - 1);
 
-		transform->switchTransform(cmd, params);
-    } 
+        // Trim whitespace
+        func.erase(0, func.find_first_not_of(" \t\n\r"));
+        func.erase(func.find_last_not_of(" \t\n\r") + 1);
+
+        vector<float> params = parseTransformParams(paramsStr);
+        transform->parseTransformFunction(func, params);
+    }
+
+    if (!transform->useMatrix) {
+        delete transform;
+        return nullptr;
+    }
 
     return transform;
 }
 
-void Transform::switchTransform(const string& cmd, const vector<float>& params) {
-    if (cmd == "translate" && (params.size() == 1 || params.size() == 2)) {
-        float tx = params[0];
-        float ty = (params.size() == 2) ? params[1] : 0.0f;
-        setTranslateX(tx);
-        setTranslateY(ty);
-    }
-    else if (cmd == "scale" && (params.size() == 1 || params.size() == 2)) {
-        float sx = params[0];
-        float sy = (params.size() == 2) ? params[1] : sx;
-        setScaleX(sx);
-        setScaleY(sy);
-    }
-    else if (cmd == "rotate" && (params.size() == 1 || params.size() == 3)) {
-        float angle = params[0];
-        if (params.size() == 3) {
-            float cx = params[1];
-            float cy = params[2];
-            setRotation(angle, cx, cy);
-        }
-        else {
-            setRotation(angle);
-        }
-    }
-    else if (cmd == "matrix" && params.size() == 6) {
-        setMatrix(params[0], params[1], params[2], params[3], params[4], params[5]);
-    }
-}
-
-vector<float> parseParams(const string& paramsStr) {
+vector<float> parseTransformParams(const string& str) {
     vector<float> params;
-    istringstream ss(paramsStr);
-    string param;
-    while (ss >> param) {
-        params.push_back(stof(param));
+    string cleaned = str;
+    
+    for (size_t pos = 0; pos < cleaned.length(); ++pos) {
+        if (cleaned[pos] == ',') {
+            cleaned[pos] = ' ';
+        } 
+        else if (cleaned[pos] == '-' && pos > 0 && cleaned[pos - 1] != ' ' && cleaned[pos - 1] != '(') {
+			cleaned.insert(pos, " ");
+            pos++;
+		}
+	}
+
+	istringstream ss(cleaned);
+    float value;
+    while (ss >> value) {
+		params.push_back(value);
     }
+
     return params;
+
 }
 
-vector<string> split(const string& str) {
+vector<string> splitTransforms(const string& str) {
     vector<string> tokens;
+    size_t i = 0;
 
-    int i = 0;
     while (i < str.length()) {
-        
-		i = str.find_first_not_of(" \t\n", i);
-        if (i == string::npos) break;
-        
-		int j = str.find_first_of(" (", i);
-		if (j == string::npos) break;
+        // Skip whitespace
+        while (i < str.length() && isspace(str[i])) {
+            i++;
+        }
+        if (i >= str.length()) break;
 
-		int k = str.find_first_of(")", j);
-		if (k == string::npos) break;
+        // Find function name
+        size_t funcStart = i;
+        while (i < str.length() && str[i] != '(') {
+            i++;
+        }
+        if (i >= str.length()) break;
 
-		string cmd = str.substr(i, k - i + 1);
-		replace(cmd.begin(), cmd.end(), ',', ' ');
-		tokens.push_back(cmd);
-		i = k + 1;
+        // Find matching closing parenthesis
+        size_t parenStart = i;
+        int depth = 0;
+        while (i < str.length()) {
+            if (str[i] == '(') depth++;
+            else if (str[i] == ')') {
+                depth--;
+                if (depth == 0) {
+                    i++;
+                    break;
+                }
+            }
+            i++;
+        }
+
+        tokens.push_back(str.substr(funcStart, i - funcStart));
     }
+
     return tokens;
+}
+
+Gdiplus::Matrix* Transform::getMatrix() const {
+    return matrix;
+}
+
+Transform& Transform::operator=(const Transform& other) {
+    if (this != &other) {
+        if (matrix) {
+            delete matrix;
+            matrix = nullptr;
+        }
+        useMatrix = other.useMatrix;
+        if (other.matrix) {
+            matrix = other.matrix->Clone();
+        }
+    }
+    return *this;
 }
--- SVGDemo/Transform.h
+++ SVGDemo/Transform.h
@@ -1,3 +1,59 @@
+//#ifndef TRANSFORM_H
+//#define TRANSFORM_H
+//#include <gdiplus.h>
+//#include <string>
+//#include <vector>
+//
+//class Transform
+//{
+//private:
+//    float scaleX;
+//    float scaleY;
+//    float rotation;
+//    float translateX;
+//    float translateY;
+//
+//    Gdiplus::Matrix* matrix;
+//    bool useMatrix;
+//
+//public:
+//    Transform(float sx = 1.0f, float sy = 1.0f,
+//        float rot = 0.0f,
+//        float tx = 0.0f, float ty = 0.0f);
+//
+//    virtual ~Transform();
+//
+//    // Áp dụng transform lên Graphics
+//    void Apply(Gdiplus::Graphics& g) const;
+//
+//    // Hỗ trợ
+//    //Gdiplus::Matrix ToMatrix() const;
+//
+//    // Getter
+//    float getScaleX() const { return scaleX; }
+//    float getScaleY() const { return scaleY; }
+//    float getRotation() const { return rotation; }
+//    float getTranslateX() const { return translateX; }
+//    float getTranslateY() const { return translateY; }
+//
+//    void setScaleX(float);
+//    void setScaleY(float);
+//    void setRotation(float);
+//    void setRotation(float, float, float);
+//    void setTranslateX(float);
+//    void setTranslateY(float);
+//    void setMatrix(float, float, float, float, float, float);
+//
+//    void switchTransform(const std::string&, const std::vector<float>&);
+//
+//    static Transform* parse(const std::string&);
+//};
+//
+//std::vector<std::string> split(const std::string&);
+//std::vector<float> parseParams(const std::string&);
+//
+//#endif // !TRANSFORM_H
+
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
 #include <gdiplus.h>
@@ -7,49 +63,37 @@
 class Transform
 {
 private:
-    float scaleX;
-    float scaleY;
-    float rotation;
-    float translateX;
-    float translateY;
-
-	Gdiplus::Matrix* matrix;
+    Gdiplus::Matrix* matrix;
     bool useMatrix;
 
 public:
-    Transform(float sx = 1.0f, float sy = 1.0f,
-              float rot = 0.0f,
-              float tx = 0.0f, float ty = 0.0f);
-
+    Transform();
+    Transform(const Transform&);
     virtual ~Transform();
 
-    // Áp dụng transform lên Graphics
     void Apply(Gdiplus::Graphics& g) const;
 
-    // Hỗ trợ
-    //Gdiplus::Matrix ToMatrix() const;
+    // Transform operations - these build the matrix
+    void addTranslate(float tx, float ty);
+    void addScale(float sx, float sy);
+    void addRotate(float angle);
+    void addRotate(float angle, float cx, float cy);
+    void addMatrix(float m11, float m12, float m21, float m22, float dx, float dy);
+    void addSkewX(float angle);
+    void addSkewY(float angle);
 
-    // Getter
-    float getScaleX() const { return scaleX; }
-    float getScaleY() const { return scaleY; }
-    float getRotation() const { return rotation; }
-    float getTranslateX() const { return translateX; }
-    float getTranslateY() const { return translateY; }
+    static Transform* parse(const std::string& transformStr);
 
-    void setScaleX(float);
-	void setScaleY(float);
-	void setRotation(float);
-	void setRotation(float, float, float);
-	void setTranslateX(float);
-    void setTranslateY(float);
-	void setMatrix(float, float, float, float, float, float);
+    Gdiplus::Matrix* getMatrix() const;
 
-    void switchTransform(const std::string&, const std::vector<float>&);
+    Transform& operator=(const Transform&);
 
-	static Transform* parse(const std::string&);
+private:
+    void ensureMatrix();
+    void parseTransformFunction(const std::string& func, const std::vector<float>& params);
 };
 
-std::vector<std::string> split(const std::string&);
-std::vector<float> parseParams(const std::string&);
+std::vector<std::string> splitTransforms(const std::string& str);
+std::vector<float> parseTransformParams(const std::string& str);
 
 #endif // !TRANSFORM_H
